841b073e22e9243e8f27ed792e7d76b5c1e37217
Ori Kerer <orikerergil@gmail.com>
2023-07-20 01:36:03 +0300

    Working generation of Isar collection from generated class

--- packages/isar_crdt_generator/lib/isar_crdt_generator.dart
+++ packages/isar_crdt_generator/lib/isar_crdt_generator.dart
@@ -1,11 +1,17 @@
 import 'package:build/build.dart';
 import 'package:source_gen/source_gen.dart';
+import 'package:isar_generator/src/collection_generator.dart';
 
 import 'src/isar_crdt_generator_base.dart';
 
-Builder generateCrdtCollection(BuilderOptions options) => SharedPartBuilder(
+Builder generateCrdtCollection(BuilderOptions options) => PartBuilder(
       [
         CrdtCollectionGenerator(),
       ],
-      'isar_crdt_generator',
+      '.crdt.dart',
     );
+
+// Builder generateCollection(BuilderOptions options) => PartBuilder(
+//       [IsarCollectionGenerator()],
+//       'isar_collection_generator',
+//     );

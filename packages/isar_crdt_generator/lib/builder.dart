a1ab5651985b56c721187bc6cf81d6fce075a878
Ori Kerer <orikerergil@gmail.com>
2023-07-20 12:04:29 +0300

    rename

--- packages/isar_crdt_generator/lib/builder.dart
+++ packages/isar_crdt_generator/lib/builder.dart
@@ -1,7 +1,7 @@
 import 'package:build/build.dart';
 import 'package:source_gen/source_gen.dart';
 
-import 'src/isar_crdt_generator_base.dart';
+import 'src/crdt_collection_generator.dart';
 
 Builder generateCrdtCollection(BuilderOptions options) => PartBuilder(
       [
